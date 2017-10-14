08ac4bc13b025fecddc32ff4e82a0d3c882bad1e
Michelle Lockett <shelldog42@gmail.com>
2017-10-14 12:18:01 -0700

    Pass all map tests with pomander

--- src/underbar.js
+++ src/underbar.js
@@ -138,6 +138,14 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+
+    var results = [];
+
+    _.each(collection, function(value) {
+      results.push(iterator(value));
+    });
+
+    return results;
   };
 
   /*
