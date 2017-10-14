1e23af02f5974887ea06368a09f42b8030fef6f0
Michelle Lockett <shelldog42@gmail.com>
2017-10-14 11:24:20 -0700

    Pass reject tests with pomander

--- src/underbar.js
+++ src/underbar.js
@@ -85,7 +85,6 @@
         result = index;
       }
     });
-
     return result;
   };
 
@@ -107,6 +106,10 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(element) {
+      return !test(element);
+    });
+
   };
 
   // Produce a duplicate-free version of the array.
