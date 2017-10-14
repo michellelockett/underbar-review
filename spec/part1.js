fd194acc2273978483e21dac931aa44e7273b95a
Michelle Lockett <shelldog42@gmail.com>
2017-10-14 14:59:55 -0700

    Adds test behavior

--- spec/part1.js
+++ spec/part1.js
@@ -502,7 +502,7 @@
         expect(result).to.equal(4);
       });
 
-      it('Fill me in with a description of the behavior this test is checking for', function() {
+      it('should allow falsy accumulators', function() {
         var result = _.reduce([1, 2, 3], function(memo, item) {
 
           return memo * item;
